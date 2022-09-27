26bc986ed327372a72088d34626010e95754e0f4
oNEOBYTEo <andersoncifuentesdev@gmail.com>
2022-09-27 10:33:12 -0500

    fix layout and add forms

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,5 +1,5 @@
-import { Button, Grid, Typography } from '@mui/material';
-import Image from 'next/image';
+import { Box, Grid, Typography } from '@mui/material';
+import AccessibleForwardIcon from '@mui/icons-material/AccessibleForward';
 
 export const Layout = ({ children }) => {
   return (
@@ -7,24 +7,39 @@ export const Layout = ({ children }) => {
       container
       sx={{ width: '100%', height: '100vh' }}
       justifyContent="space-between"
+      spacing={0}
     >
-      <Grid item xs={7}>
-        <Typography variant="h3" component="h2">
-          <Image
-            src="https://images.unsplash.com/photo-1607799013470-8a46c0db7eb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
-            alt="hospital building"
-            width={50}
-            height={50}
-          />
-          Hospital Name
-        </Typography>
-        {children}
+      <Grid item xs={7} display="flex" padding={3}>
+        <Box sx={{ position: 'absolute' }}>
+          <Typography variant="h3" component="h2">
+            <AccessibleForwardIcon sx={{ fontSize: 40 }} color="primary" />
+            Hospital Name
+          </Typography>
+        </Box>
+        <Grid
+          justifyContent={'center'}
+          alignItems={'center'}
+          display="flex"
+          flexGrow={1}
+        >
+          {children}
+        </Grid>
       </Grid>
-      <Grid item xs={5} sx={{ width: '100%' }}>
+      <Grid
+        item
+        xs={5}
+        overflow="hidden"
+        height={'100vh'}
+        // sx={{
+        //   backgroundImage:
+        //     'url(https://images.unsplash.com/photo-1607799013470-8a46c0db7eb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80)',
+        //   objectFit: 'contain',
+        // }}
+      >
         <img
           src="https://images.unsplash.com/photo-1607799013470-8a46c0db7eb0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
-          alt="hospital building"
-          style={{ maxWidth: '100%', height: '100vh' }}
+          alt="coss"
+          width={'100%'}
         />
       </Grid>
     </Grid>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,16 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import '../styles/globals.css';
+import theme from '../theme/theme';
 // import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
