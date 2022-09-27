6cc6e109421127b142ad76ad0ff5fb30a8b8e508
pablo-zalazar <pablo_zalazar@outlook.com>
2022-09-27 12:19:33 -0300

    middleware fix

--- src/middleware.js
+++ src/middleware.js
@@ -2,22 +2,26 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
 export default async function middleware(req, res) {
-  // const jwt = req.cookies.get("myTokenName");
-  // console.log(jwt);
-
-  try {
-    const token = req.headers.get("authorization").split(" ")[1];
-    console.log(token);
-    const { payload } = await jwtVerify(
-      token,
-      new TextEncoder().encode(process.env.JWT_SECRET)
-    );
-    console.log({ payload });
-    const id = payload.id;
-    console.log(id);
-    return NextResponse.next();
-  } catch (e) {
-    console.log(e.message);
+  let token;
+  if (
+    req.headers.get("authorization") &&
+    req.headers.get("authorization").startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.get("authorization").split(" ")[1];
+      const { payload } = await jwtVerify(
+        token,
+        new TextEncoder().encode(process.env.JWT_SECRET)
+      );
+      const id = payload.id;
+      return NextResponse.next();
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
+  if (token) {
+    const error = new Error("Invalid token");
+    return res.status(400).json({ msg: error.message });
   }
 }
 
