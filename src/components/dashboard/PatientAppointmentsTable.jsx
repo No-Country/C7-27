43c9ae8f6790eb34309f1ffcca88234485e0a968
oNEOBYTEo <andersoncifuentesdev@gmail.com>
2022-10-14 17:43:10 -0500

    cancel appointments in dashboard main

--- src/components/dashboard/PatientAppointmentsTable.jsx
+++ src/components/dashboard/PatientAppointmentsTable.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { capitalize } from '@mui/material';
+import {
+  Box,
+  capitalize,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@mui/material';
 import {
   Table,
   TableBody,
@@ -12,44 +20,122 @@ import {
 } from '../../components/auth';
 
 import { SeverityPill } from './SeverityPill';
+import axios from 'axios';
 
 export function PatientAppointmentsTable({ appointments }) {
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [id, setId] = useState('');
+
+  const updateAppointment = () => {
+    axios
+      .put(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/appointments/updateAppointment`,
+        {
+          id,
+          confirmed: false,
+        }
+      )
+      .finally(() => {
+        setId('');
+        location.reload();
+      });
+  };
+
+  const onClick = async (data) => {
+    setId(data._id);
+    setShowOverlay(true);
+  };
+
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Professional</TableCell>
-          <TableCell>Speciality</TableCell>
-          <TableCell sortDirection="desc">
-            <Tooltip enterDelay={300} title="Sort">
-              <TableSortLabel active direction="desc">
-                Date
-              </TableSortLabel>
-            </Tooltip>
-          </TableCell>
-          <TableCell>Status</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {appointments?.map((appointment) => (
-          <TableRow hover key={appointment._id}>
-            <TableCell>
-              {`
+    <Box sx={{ position: 'relative' }}>
+      {showOverlay && (
+        <Card
+          sx={{
+            position: 'absolute',
+            backgroundColor: '#0009',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <CardContent>
+            <Typography
+              variant="h5"
+              component="h3"
+              textAlign={'center'}
+              color={'white'}
+            >
+              Are your sure to delete this appointment?
+            </Typography>
+          </CardContent>
+          <CardActions
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              width: '100%',
+              maxWidth: '400px',
+            }}
+          >
+            <Button
+              size="small"
+              variant={'contained'}
+              color="secondary"
+              onClick={updateAppointment}
+            >
+              CONFIRM
+            </Button>
+            <Button
+              size="small"
+              variant={'contained'}
+              color="error"
+              onClick={() => setShowOverlay(false)}
+            >
+              CANCEL
+            </Button>
+          </CardActions>
+        </Card>
+      )}
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Professional</TableCell>
+            <TableCell>Speciality</TableCell>
+            <TableCell sortDirection="desc">
+              <Tooltip enterDelay={300} title="Sort">
+                <TableSortLabel active direction="desc">
+                  Date
+                </TableSortLabel>
+              </Tooltip>
+            </TableCell>
+            <TableCell>Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {appointments?.map((appointment) => (
+            <TableRow hover key={appointment._id}>
+              <TableCell>
+                {`
               ${capitalize(appointment.professionalRef.lastName)}
               ${capitalize(appointment.professionalRef.firstName)}`}
-            </TableCell>
-            <TableCell>{appointment.professionalRef.speciality}</TableCell>
-            <TableCell>{appointment.date}</TableCell>
-            <TableCell>
-
-              <SeverityPill color={appointment.confirmed ? 'success' : 'error'}>
-                {appointment.confirmed ? 'Confirmed' : 'Cancelled'}
-
-              </SeverityPill>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+              </TableCell>
+              <TableCell>{appointment.professionalRef.speciality}</TableCell>
+              <TableCell>{appointment.date}</TableCell>
+              <TableCell>
+                <SeverityPill
+                  color={appointment.confirmed ? 'success' : 'error'}
+                  onClick={() => onClick(appointment)}
+                  sx={{ cursor: 'pointer' }}
+                >
+                  {appointment.confirmed ? 'Confirmed' : 'Cancelled'}
+                </SeverityPill>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Box>
   );
 }
